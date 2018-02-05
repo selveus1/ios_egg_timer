20c86b670af2af65bf73a0e1fe83defa93239a9b
selveus1 <fs031090@gmail.com>
2018-02-05 13:04:33 -0500

    fixed a display error

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,11 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let defaultTime = 210
+var time = 210
+//var isStopped = true
 
+class ViewController: UIViewController {
+    
+    var timer = Timer() //creating a Timer obj
+    @IBOutlet weak var timerLabel: UILabel!
+
+    
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        print("Stopped")
+        timer.invalidate()
+        timerLabel.text = "\(time)"
+    }
+    
+    @IBAction func playBtnPressed(_ sender: Any) {
+        timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func resetBtnPressed(_ sender: Any) {
+        timerLabel.text = "\(defaultTime)"
+        time = defaultTime
+        timer.invalidate()
+    }
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+ 
+        if( time < defaultTime && (time + 10) < defaultTime){
+            time = time + 10
+            timerLabel.text = "\(time)"
+        }else{
+            timerLabel.text = "\(defaultTime)"
+        }
+    }
+    
+    @IBAction func minusBtnPressed(_ sender: Any) {
+        if( time > 0 && (time - 10) > 0){
+            time = time - 10
+            timerLabel.text = "\(time)"
+        }else{
+            timerLabel.text = "\(0)"
+            timer.invalidate()
+        }
+    }
+    
+    
+    @objc func processTimer(){
+        print("A second has passed --> \(time)" )
+        time = time - 1
+        timerLabel.text = "\(time)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(processTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
